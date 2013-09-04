af718c010df712ab8107378af121ff835ec7ecc7
scrathe <scrathe@users.noreply.github.com>
2013-09-03 19:14:51 -0500

    Update plink-tardisIVR.sh

--- plink-tardisIVR.sh
+++ plink-tardisIVR.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# use full path to script
+# the $win_path directories defined below are the only location right-click-to-plink works
+# improve this
+
+# ~/.sabnzbd   # tidle ~/ home directory reference doesn't seem to work
+# use full path to script instead
 script="/home/elvie/.sabnzbd/scripts/tardisIVR/tardisIVRvideo.sh"
 
 if [[ "$2" == "movies" ]]; then
+    # Movies
+    # windows/host is serving storage
+    # setup your windows/source to linux/mount storage paths
     win_path="X:\\\media\\\Movies"
     lnx_path="/media/tardis-x/media/Movies"
+    
+    # tag / atomicparsley only
     if [[ "$3" == "tag" ]]; then
         options="x x x movies x x tag"
     else
+    # handbrake + atomicparsley
         options="x x x movies x x"
     fi
 elif [[ "$2" == "tv" ]]; then
+    # TV
     win_path="X:\\\media\\\TV"
     lnx_path="/media/tardis-x/media/TV"
+    
     if [[ "$3" == "tag" ]]; then
         options="x x x tv x x tag"
     else
@@ -24,7 +36,13 @@ else
    exit 1
 fi
 
+# remap your windows to linux storage paths
+# $1 = argument passed from windows/plink-tardisIVR.bat script
 dir=$(sed "s|"$win_path"|"$lnx_path"|g" <<< $1)
+
+# convert windows \ slashes to linux / slashes
 dir=$(sed 's|\\|\/|g' <<< $dir)
+
+# send command to tardisIVRvideo.sh
 echo "'$script' '$dir' '$options'"
 "$script" "$dir" $options
